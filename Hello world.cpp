5d77a527f5d3aa82a19c77cd08e8d2689c84dbe1
Your Name <you@example.com>
2020-09-01 20:19:50 +0530

    Final updated dev branch

--- Hello world.cpp
+++ Hello world.cpp
@@ -6,6 +6,7 @@ int main() {
 	printf("Hello world !");
 	printf("Hi");  // so this is the change which we have to do
 	printf("Changes in dev branch");
+	printf("Final changes in dev branch");
 	
 	
 	
